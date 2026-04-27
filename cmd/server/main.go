63a96fddef1fec02d167fc2707dbfdb78029dddd
Pat Trudel <patrick.trudel@me.com>
2026-04-27 13:28:29 -0700

    feat: graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
+	"time"
 
 	"github.com/itsPat/go-url-shortener/internal/httpapi"
 	"github.com/itsPat/go-url-shortener/internal/links"
@@ -20,7 +25,7 @@ func main() {
 
 	envErr := godotenv.Load()
 	if envErr != nil {
-		slog.Error("Failed to load env file", "err", envErr)
+		slog.Error("failed to load env file", "err", envErr)
 		os.Exit(1)
 	}
 
@@ -32,23 +37,59 @@ func main() {
 
 	db, dbErr := gorm.Open(postgres.Open(dbUri), &gorm.Config{})
 	if dbErr != nil {
-		slog.Error("DB Error", "err", dbErr)
+		slog.Error("db error", "err", dbErr)
 		os.Exit(1)
 	}
 
 	migrationErr := db.AutoMigrate(&links.Link{})
 	if migrationErr != nil {
-		slog.Error("DB Migration Error", "err", migrationErr)
+		slog.Error("db migration error", "err", migrationErr)
 		os.Exit(1)
 	}
 
 	linkStore := links.NewDatabase(db)
 
 	server := httpapi.NewServer(linkStore)
-	slog.Info("Server Listening", "port", port)
-	serverErr := http.ListenAndServe(":"+strconv.Itoa(port), server.Handler())
-	if serverErr != nil {
-		slog.Error("Server Error", "err", serverErr)
-		os.Exit(1)
+	srv := &http.Server{
+		Addr:    ":" + strconv.Itoa(port),
+		Handler: server.Handler(),
 	}
+
+	go func() {
+		slog.Info("server listening", "port", port)
+		serverErr := srv.ListenAndServe()
+
+		if !errors.Is(serverErr, http.ErrServerClosed) {
+			slog.Error("server error", "err", serverErr)
+			os.Exit(1)
+		}
+	}()
+
+	// Listen for stop signals
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	<-ctx.Done()
+
+	slog.Info("server shutting down")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Stop accepting requests
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		slog.Error("server shutdown error", "err", err)
+	} else {
+		slog.Info("server shutdown gracefully")
+	}
+
+	// Close DB
+	sqlDB, err := db.DB()
+	if err != nil {
+		slog.Error("get db handle error", "err", err)
+		return
+	}
+	if err := sqlDB.Close(); err != nil {
+		slog.Error("db close error", "err", err)
+	}
+	slog.Info("db shutdown gracefully")
 }
